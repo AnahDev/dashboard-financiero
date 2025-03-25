e970b1637ef3cbdc1f89facb931009641b97ae1f
Anahdev <anadevtools@gmail.com>
2025-03-24 21:54:36 -0400

    features

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -8,13 +8,20 @@ import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 
 //import functions
-import { fetchRevenue } from "@/app/lib/data";
-import { fetchLatestInvoices } from "@/app/lib/data";
+// import { fetchRevenue } from "@/app/lib/data"; //
+// suplantadas por react suspense (streaming en componente)
+
+// import { fetchLatestInvoices } from "@/app/lib/data";
 import { fetchCardData } from "@/app/lib/data";
+import { Suspense } from "react";
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from "@/app/ui/skeletons";
 
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -39,8 +46,15 @@ export default async function Page() {
       </div>
 
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        {/* <RevenueChart revenue={revenue} />
+         */}
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
+        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
   );
